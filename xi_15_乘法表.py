8e9bc8deddcdaa1a19e6c0e3f9b0519eb798656f
xuxu <xu829475@qq.com>
2019-12-10 17:40:37 +0800

    循环

--- xi_15_乘法表.py
+++ xi_15_乘法表.py
@@ -2,7 +2,7 @@ row = 1
 while row <= 9:
     col = 1
     while col <= row:
-        print("%d * %d = %d" % (col,row,row*col),end="   ")
+        print("%d * %d = %2d" % (col,row,row*col),end="   ")
         col += 1
     print("")
     row += 1
